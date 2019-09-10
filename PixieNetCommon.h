9f6cb6d38eb9f310bfa64cd22a65973d7f91cda1
whennig <whennig@xia.com>
2019-09-10 17:57:02 +0000

    modified   PixieNetCommon.h - MSU changes for C++

--- PixieNetCommon.h
+++ PixieNetCommon.h
@@ -35,20 +35,30 @@
 
 // refs for shared functions
 
-int read_print_runstats(int mode, int dest, volatile unsigned int *mapped );
-int read_print_runstats_XL_2x4(int mode, int dest, volatile unsigned int *mapped );
-void I2Cstart(volatile unsigned int *mapped);
-void I2Cstop(volatile unsigned int *mapped);
-void I2Cslaveack(volatile unsigned int *mapped);
-void I2Cmasterack(volatile unsigned int *mapped);
-void I2Cmasternoack(volatile unsigned int *mapped);
-void I2Cbytesend(volatile unsigned int *mapped, unsigned int *data);
-void I2Cbytereceive(volatile unsigned int *mapped, unsigned int *data);
-unsigned int setbit( unsigned int par, unsigned int bitc, unsigned int bitf);
-unsigned int hwinfo( volatile unsigned int *mapped,unsigned int I2Csel );
-float board_temperature( volatile unsigned int *mapped, unsigned int I2Csel  );
-float zynq_temperature();
-int ADCinit_DB01(volatile unsigned int *mapped );
+#pragma once
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+  
+  int read_print_runstats(int mode, int dest, volatile unsigned int *mapped );
+  int read_print_runstats_XL_2x4(int mode, int dest, volatile unsigned int *mapped );
+  void I2Cstart(volatile unsigned int *mapped);
+  void I2Cstop(volatile unsigned int *mapped);
+  void I2Cslaveack(volatile unsigned int *mapped);
+  void I2Cmasterack(volatile unsigned int *mapped);
+  void I2Cmasternoack(volatile unsigned int *mapped);
+  void I2Cbytesend(volatile unsigned int *mapped, unsigned int *data);
+  void I2Cbytereceive(volatile unsigned int *mapped, unsigned int *data);
+  unsigned int setbit( unsigned int par, unsigned int bitc, unsigned int bitf);
+  unsigned int hwinfo( volatile unsigned int *mapped,unsigned int I2Csel );
+  float board_temperature( volatile unsigned int *mapped, unsigned int I2Csel  );
+  float zynq_temperature();
+  int ADCinit_DB01(volatile unsigned int *mapped );
+
+#ifdef __cplusplus
+}
+#endif
 
 
 
